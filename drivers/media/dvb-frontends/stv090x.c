beb8e0836a3b79cf808cf848183c08df848fbb43
Christopher Lee <updatelee@gmail.com>
2017-11-26 11:18:37 -0700

    set min SR to 100,000 for stv090x, the datasheet doesnt actually specify a min sr just says that its been tested from <1Msps to 45Msps

--- drivers/media/dvb-frontends/stv090x.c
+++ drivers/media/dvb-frontends/stv090x.c
@@ -5426,7 +5426,7 @@ static const struct dvb_frontend_ops stv090x_ops = {
 		.frequency_max 		= 2150000,
 		.frequency_stepsize	= 0,
 		.frequency_tolerance	= 0,
-		.symbol_rate_min 	= 1000000,
+		.symbol_rate_min 	= 100000,
 		.symbol_rate_max 	= 45000000,
 		.caps			= FE_CAN_INVERSION_AUTO |
 					  FE_CAN_FEC_AUTO       |
