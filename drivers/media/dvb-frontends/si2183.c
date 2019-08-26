smp79/v4l-updatelee

commit f4129d1ef7d22289ade8d809d759efc9d6d10505

[media] si2183: fix si2183_get_frontend_algo()'s return type. The function dvb_frontend_ops::get_frontend_algo() is defined as returning an 'enum dvbfe_algo', but the implementation in this driver returns an 'int'. Fix this by returning 'enum dvbfe_algo' in this driver too.

drivers/media/dvb-frontends/si2183.c
@@ -1082,7 +1082,7 @@ static int si2183_tune(struct dvb_frontend *fe, bool re_tune,
 	return si2183_read_status(fe, status);
 }
 
-static int si2183_get_frontend_algo(struct dvb_frontend *fe)
+static dvbfe_algo si2183_get_frontend_algo(struct dvb_frontend *fe)
 {
 	struct i2c_client *client = fe->demodulator_priv;
 	struct si2183_config *config = client->dev.platform_data;

commit e8ebcd3f67b3a5a9a5b6a223c5ccfbfba64c1b3a

[media] si2183: fix typo in si2183_get_frontend_algo introduced in commit f4129d1ef7, oops

drivers/media/dvb-frontends/si2183.c
@@ -1082,7 +1082,7 @@ static int si2183_tune(struct dvb_frontend *fe, bool re_tune,
 	return si2183_read_status(fe, status);
 }
 
-static dvbfe_algo si2183_get_frontend_algo(struct dvb_frontend *fe)
+static enum dvbfe_algo si2183_get_frontend_algo(struct dvb_frontend *fe)
 {
 	struct i2c_client *client = fe->demodulator_priv;
 	struct si2183_config *config = client->dev.platform_data;